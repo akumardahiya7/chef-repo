638c40d4a2616b85276e16446f939217640d270c
Anil Dahiya <anildahiya@hashmapinc.com>
2018-02-12 10:55:36 +0000

    updated ambari-server

--- roles/ambari-server.rb
+++ roles/ambari-server.rb
@@ -1,5 +1,5 @@
-name "ambari-server"
+name "hdp-worker"
 description "A role to configure node as HDP worker node"
 #run_list "recipe[selinux]", "recipe[basic-packages]", "recipe[ssh-keys]"
-run_list "recipe[selinux]", "recipe[hdp_envsetup]", "recipe[firewalld]", "recipe[ssh-keys]"
+run_list "recipe[selinux]", "recipe[hdp_envsetup]", "recipe[firewalld]", "recipe[ssh-keys]", "recipe[ambari::server]", "recipe[ambari::agent]"
 default_attributes "ssh_keys" => { "cddadmin" => "cddadmin" }
